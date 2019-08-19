62f1fe37329e6d4af97f55ee8b1f0f4c44d5e4c0
Leonid Nikolaev <nikonor@nikonor.ru>
2019-08-19 10:59:57 +0300

    Second commit

--- m.go
+++ m.go
@@ -5,7 +5,7 @@ import "unicode"
 func AwesomeString(in string) string {
     out := make([]rune,len(in))
     for i,ch := range in {
-        if i % 2 != 0 {
+        if i % 2 == 0 {
             out[i] =  unicode.ToUpper(ch)
         } else {
             out[i] =  unicode.ToLower(ch)
