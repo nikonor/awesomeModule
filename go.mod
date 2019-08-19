87f21bb958b2120f770314a729f797d1c9423b86
Leonid Nikolaev <nikonor@nikonor.ru>
2019-08-19 10:52:12 +0300

    First commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/nikonor/awesomeModule
+
+go 1.12
---
+++ m.go
@@ -0,0 +1,16 @@
+package awesomeModule
+
+import "unicode"
+
+func AwesomeString(in string) string {
+    out := make([]rune,len(in))
+    for i,ch := range in {
+        if i % 2 != 0 {
+            out[i] =  unicode.ToUpper(ch)
+        } else {
+            out[i] =  unicode.ToLower(ch)
+        }
+    }
+    
+    return string(out)
+}
